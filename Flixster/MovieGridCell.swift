186397f8d7d97de351496c2cc10a71aae1ba46b6
abdallahy271 <abdallahy271@hmail.com>
2020-09-28 19:27:03 -0700

    Flixster App part 2

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCellCollectionViewCell.swift
+//  Flixster
+//
+//  Created by MAC on 9/28/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
